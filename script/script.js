9015fe241c0bf1784cdb291ee863adbc1627336f
inna-m9 <mkrtchyaninna4@gmail.com>
2023-02-08 19:49:18 +0400

    project

---
+++ script/script.js
@@ -0,0 +1,765 @@
+// const arr= new Array(10);
+// console.log(arr);
+
+// arr[0]='barev';
+// arr[1]=7;
+// console.log(arr);
+
+// const arr2=[78,24,90, 'hello'];
+// const gurgen=['yogurt'];
+// const zhenya = gurgen;
+// console.log(zhenya);
+// console.log(arr2);
+// arr2[1]=102;
+// console.log(arr2[1]);
+
+
+// const arr5=[78,14,25,76,35,6];
+// arr5.shift();
+
+// console.log(arr5);
+
+// for(let i=0; i<arr5, lenght;i++){
+//   console.log(i);
+// }
+
+
+// newaArr = [];
+// for(let n=1; n<=100; n++);{
+//   if (n%x===0);
+//   console.log ();
+// }
+
+// let arr=[78,13,25,6,25];
+// function compare(a,b){
+//   if(a>b){
+//     return 1
+//   } else if(a<b){
+//     return -1;
+//  }else{
+//   return 0;
+//  }
+// }
+// arr.sort(compare)
+// console.log(arr);
+
+
+// let arr = [];
+// function f1 (n){
+//   let fact =1
+//   for(let i=1; i<=n; i++){
+//     fact= fact*i
+//   }
+// }
+// console.log(i); // nael
+
+// const arr = [5, 15, 45];
+
+// let sum = 0;
+
+// for (const value of arr) {
+//   sum += value;
+// }
+
+// console.log(sum);
+
+// const arr=[5, 22, 28, 14, 62];
+// function sum {
+//   if(arr>20)
+//   console.log(sum);
+// }
+
+
+// function sumArray{
+//   const arr=[5, 22, 28, 14, 62];
+// let sum=0;
+// for (let i=0; i>arr; i+=1){
+//   sum+=arr[i];
+//   console.log(sumarr);
+// }
+// }  //nael
+
+
+// for (int i = 1; i < n; ++i) {
+//   if (arr[0] < arr[i]) {
+//     arr[0] = arr[i];
+//   }
+// }
+
+// function findlargest(Array)
+// const arr = [3, 8, 55, 78, 20, 100];
+// for (let i= 0; i< arr.length; i++) 
+//   if (arr[i] === largest) {
+//     console.log(i);
+//   }
+
+
+// const items = [27, 65, 98];
+// items.reverse();
+// console.log(items);
+
+
+// function countduplicates(Array)
+// let arr = [5,4,5,7,8,9,5,7,5];
+// let counts=[];
+// console.log(counts);
+
+// function ArrayRemove()
+// let arr=([56,45,57,8],8);
+// remove.arr[8];
+// console.log(arr);
+
+// let arr =[67, 41,4,1,413];
+// for(let item of arr){
+//     console.log(item);
+// }
+
+
+
+// let arr = [28,48,35,44];
+// let sum=0;
+
+// for (let item of arr){
+    
+//     if (item>=20 || item<=50 ){
+//         sum+=item
+
+//     }
+// }
+
+// console.log(sum);   // 20ic 50 mijakaykum tveri gumare
+
+
+
+// function b(a){
+//     if(Array.isArray(a)){
+//         a.forEach((item)=>){
+//             console.log(item);
+//         }
+//     }
+// }
+// b([89,13,35,7,2,52])
+
+
+// let arrMatrix=[[56,3,13,13], 'hello',[89,313,4]89,67];
+// for (let i=0; i<arrMatrix.length; i++){
+//     if(ArrayisArray(arrMatrix[i])){
+//         console.log(arrMatrix[i]);
+//     }
+// }
+
+
+// let arrAll=[[87,23,55,4,12], 98,78];
+// let zuyg=0;
+// for(let i=0; i<=arrAll.length; i++)
+// if(Array.isArray(arrAll[i])){
+// arrAll[i].filter(i=>i%2===0)
+// console.log(arrAll[i].filter(i=>i%2===0));
+// }
+
+
+// let arr = ([[1,2,8,],[3,7,9],[28,23,12]])
+// let sum=arr.reduce((a,b,c)=>a+b+c[0])
+// console.log(sum);
+
+
+
+// let array = [[3, 4, 6],[7, 8, 19],[89,10,2]];
+// let res=0
+// array.forEach(a=>res+=a[0])
+// console.log(res);  zangvaci arajin tarreri gumare
+
+
+
+// arr = [[1, 2, 8,],[3,7,9], [28, 23, 12]];
+// function findhighest(Array)
+// for (let i= 0; i< arr.length; i++) 
+// if(Array.isArray(arr[i]))
+//    if (arr[i] === highest) {
+//     }
+// return highest;  // chgitem
+
+
+// function highest(arr) {
+//     var highest = [[1, 2, 8,],[3,7,9], [28, 23, 12]];
+//     arr.forEach(function(i) {
+//       if (i > highest){
+//         highest = i 
+//       }
+//     })
+//    
+
+// function max(a,b,c){
+//     if(a>b && a>c){
+//         return a;
+//     }  else if (b>a && b>c){
+//         return b;
+
+//     } else{
+//         return c
+//         } 
+//     }
+
+// max(15,28,30)
+
+
+// function min(a,b,c){
+//     if(a<b && a<c){
+//         return a;
+//       }  else if (b<a && b<c){
+//             return b;
+
+//         }else { 
+//             return c
+//         }
+//     }
+//     min (12,32,8)
+//     console.log max((15,28,30))+min (12,32,8);
+
+// let arr = [4, -2, 8, -6, 9, -5];
+// function getnegativenumbers (Array)
+//  let negatives = []
+// if (Array[i]<0){
+//     multiply negatives;
+// }
+// console.log (negatives); // kisata
+
+
+// let  arr = [1, 2, 4, 5];
+// function findMissingNo (Array){
+//       let N = arr.length;
+//       let miss = findMissingNo(arr, N);}
+//       console.log(miss);  // kisata
+
+
+// let obj ={
+//       name:'Zhenya',
+//       age: 25,
+//       hair: 'long black',
+//       hands:2,
+//       images:' https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F6%2F6c%2FAbovyan_street_Gyumri.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FGyumri&tbnid=TyU5hpLM5CsBRM&vet=12ahUKEwiE8_DSirP7AhW5wQIHHZ5hADIQMygAegUIARCuAQ..i&docid=kGiqK_BsNeH-zM&w=4032&h=3024&q=lennakan&ved=2ahUKEwiE8_DSirP7AhW5wQIHHZ5hADIQMygAegUIARCuAQ'
+// }
+// console.log(obj. name);
+//  let arr= [78,13,14];
+//  let[a,b,c]=arr;
+//  console.log(c);
+
+//  let obj= {
+//       name:'Inna',
+//       lastname: 'Mkrtchyan',
+//       age: 27,
+
+//  }
+//  console.log(obj.age);
+
+
+// let obj1={
+//       sayHi:()=>{
+//             console.log('barev');
+//       },
+//       sayBye:function(){
+//             console.log('hajox');
+//       }
+// }
+// obj1.sayHi();
+// obj1.sayBye();
+
+// let ourClass=[
+//       {
+//             name:'Sveta',
+//             age:30,
+
+//       },
+//       {
+//             name:'Inna',
+//             age:27
+//       },
+//       {
+//             name:'jpitik',
+//             age: 20
+//       },
+//       {
+//             name:'Kach',
+//             age:30
+//       },
+//       {
+//             name:'Ani',
+//             age:30
+//       },
+//       {
+//             name:'Hrayr',
+//             age:'20'
+//       },
+//      { name:'Gor',
+//       age: 27
+//      }
+// ]
+// ourClass.filter(item=>console.log(item));
+
+// let ourClass=[
+//       {
+//             name:'Sveta',
+//             age:30,
+//             city: Yerevan
+            
+//       },
+//       {
+//             name:'Inna',
+//             age:27,
+//             city: Leninakan
+//       },
+//       {
+//             name:'jpitik',
+//             age: 20,
+//             city: leninakan
+//       },
+//       {
+//             name:'Kach',
+//             age:30
+//       },
+//       {
+//             name:'Ani',
+//             age:30
+//       },
+//       {
+//             name:'Hrayr',
+//             age:'20'
+//       },
+//      { name:'Gor',
+//       age: 27
+//      }
+// ]
+// ourClass.filter(item=>console.log(item));
+
+// let obj={
+//       car:'Mazda',
+//       model:'CX5',
+//       color: 'red',
+//       Year:'2022'
+
+// }  
+// for (let key in obj)
+// console.log(key);  // oka
+
+
+// let obj={
+//       name: 'Poghos',
+//       lastname: 'Petrosyan',
+//       age:'28',
+//       hometown: 'Yerevan'
+// }
+// delete obj.hometown
+// console.log(obj);   // oka
+
+
+// let laptop={
+//       name: 'asus',
+//       model: 'S15',
+//       colore: 'gray',
+//       gen: '8th'
+
+// }
+// let count=0;
+// for(let key in laptop){
+//       ++count;
+// }
+// console.log(count);  // oka
+
+
+// let people=[
+//             {
+//                   name:'Aram',
+//                   age:16,
+      
+//             },
+//             {
+//                   name:'Karen',
+//                   age:27
+//             },
+//             {
+//                   name:'Vazgen',
+//                   age: 12
+//             },
+//             {
+//                   name:'Vardan',
+//                   age:30
+//             },
+//             {
+//                   name:'Anna',
+//                   age:23
+//             },
+//             {
+//                   name:'Anush',
+//                   age:20
+//             },
+//       ]
+//       people.sort(function(a, b){ return a.age - b.age;})
+//       console.table(people);  // oka
+
+// let arr= [87,13,14,25,47,57,21,13,1314];
+// let obj={};
+// for(let i=0; i<arr.length; i++){
+//     obj[arr[i]]=arr[i]
+// }
+// console.log(obj);
+
+// let obj={
+//     name:'dddjjj'
+// }
+// obj.name;
+// obj['name']
+
+// let obj ={
+//     name: 'jirayr',
+//     age:'1'
+//     sayAge(){
+//         return obj.name
+//     }
+// };
+// console.log(obj.sayAge());
+
+
+
+// let obj2 ={
+//     name: 'malvina',
+//     age:'3'
+//     sayAge(){
+//         return obj.name
+//     }
+// };
+// function getname(){
+//     return obj.name
+// }
+// obj2.getname2=getname;
+// console.log(obj2.getname2());
+
+// function f1() {
+//     return functionf3(){
+//         console.log(this)
+//     }
+
+// }
+
+// function f1(){
+//     return this.name
+// }
+//  let obj5={
+//     name: 'Aram',
+//     age:18,
+//     photo: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fphotography%2F&psig=AOvVaw3kRw2hGDBbLqqk1hdaEG_A&ust=1668947931398000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCLC1ndehuvsCFQAAAAAdAAAAABAK'
+//  };
+ 
+//     let obj8={
+//     name: 'Garik',
+//     age: 25,
+//     photo: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.canva.com%2Fphoto-editor%2F&psig=AOvVaw3kRw2hGDBbLqqk1hdaEG_A&ust=1668947931398000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCLC1ndehuvsCFQAAAAAdAAAAABAQ'
+//  };
+
+// obj5.getname=f1;
+// obj8.getname=f1;
+
+// obj5.getname();
+// obj8.getname();
+// console.log(obj8.age);
+
+// let arr=[1,2,3,4,5,6,7,8,9,10];
+// let newaArr=arr.filter(function(item){
+    
+//         if(item % 2 ===0){
+//             return item
+//         }
+    
+    
+// })
+// console.log(newaArr);
+
+// let arr=[1,2,3,4,5]
+// let newaArr=arr.map(function(item){
+//     return item*3
+//     })
+// console.log(newaArr);
+
+
+// let arr = [1,2,3,4,6,7,7,8,10];
+// let newaArr = arr.filter(function(item){
+//      if (item %2 != 0){
+//         return item
+//      }
+// })
+// console.log(newaArr); kent tver
+
+// let arr = [1,2,3,4,6,7,7,8,10];
+
+// let sum=arr.reduce(function(aggr, val){
+//     return aggr+val
+
+// },0)
+// console.log(sum);  // arr i tveri gumare
+
+
+// arr = [1, 2, 8, 28, 23, 12];
+// function findhighest(Array)
+// for (let i= 0; i< arr.length; i++) 
+// if(Array.isArray(arr[i]))
+//    if (arr[i] === highest) {
+//     }
+//     console.log(i);
+
+// function factorialize(num) {
+//     if (num < 0) 
+//           return -1;
+//     else if (num == 0) 
+//         return 1;
+//     else {
+//         return (num * factorialize(num - 1));
+//     }
+//   }
+//   factorialize(5);
+
+
+//   function factorialize(num) {
+//     if (num === 0 || num === 1)
+//       return 1;
+//     for (var i = num - 1; i >= 1; i--) {
+//       num *= i;
+//     }
+//     return num;
+//   }
+//   factorialize(5);
+
+
+// let arr=[21,5,16,86],arr1[97,-14,43,29];
+
+
+// const a = [17, 46, 47, 98, 147, 987, 568, 72];
+// const b = [89, 765, 879, 47, 72, 658, 147, 46].concat(a);
+// let i = [];
+// for (let z = 0; z < b.length; z++) {
+//   for (let g = z + 1; g < b.length; g++) {
+//     if (b[z] === b[g]) i.push(b[z]);
+//   }
+// }
+// console.log(i);
+
+// let arr = [21,5,16,16,86,5,86],arr1=[97,-14,45,45,5,5,5,29];
+// let rez = [...arr,...arr1];
+// let obj =rez.reduce((aggr,val)=>{
+// 		if(aggr[val]===undefined){
+//     	aggr[val] = 1;
+//     }else{
+//     	aggr[val]++
+//     }
+//     return aggr
+// },{});
+// let repeatItem = []
+// for(let key in obj){
+// 		if(obj[key] > 1){
+//     	repeatItem.push(key)
+//     }
+// }
+// console.log(repeatItem)
+
+// const a = [10,20,30,40,50,60];
+// const b = [40,98,30,40,40].concat(a);
+// let i = [];
+// for (let z = 0; z < b.length; z++) {
+//   for (let g = z + 1; g < b.length; g++) {
+//     if (b[z] === b[g] && i.includes(b[z])){
+//     }
+//     else if(b[z] === b[g] ){
+//       i.push(b[z]);
+//     }
+//   }
+// }
+// console.log(i);
+
+// let arr = [21,5,16,16,86,5,86],arr1=[97,-14,45,45,5,5,5,29];
+// let rez = [...arr,...arr1];
+// let newArr = [];
+// rez.forEach((item)=>{
+// 	if(!(newArr.includes(item))){
+//   		newArr.push(item)
+//   }
+// })
+// console.log(newArr)
+
+
+// let arr = [3,4,5,6];
+// let fact = function(num){
+// 	let rez = num;
+//   for(let i =2 ; i < num;i++){
+//   		rez *=i
+//   }
+//   return rez
+// }
+// let newArr = [];
+// arr.forEach((item)=>{
+// 		newArr.push(fact(item))
+// })
+// console.log(newArr)
+
+// let matrix =[
+//     [1,2,3],
+//     [4,5,6],
+//     [7,8,9]
+
+// ]
+// let sum=0
+// for(let i=0; i<matrix.length;i++){
+//     sum=sum+matrix[i][i];
+
+// }
+// console.log(sum);
+
+
+// arr = [1,2,3,4,5,6,7,8,10,203,324,13,1];
+// function findhighest(Array)
+// for (let i= 0; i< arr.length; i++) 
+// if(Array.isArray(arr[i]))
+//    if (arr[i] === highest) {
+//     }
+//     console.log(i);
+
+
+// function countduplicates(Array)
+// let arr = [5,4,5,7,8,9,5,7,5];
+// let counts=[];
+// console.log(counts);
+
+// let block=document.querySelector('.block');
+
+// let size=10;
+// function changeSize(){
+//     size+=5;
+//     block.style.width=`${size}px`
+// }
+// const f2=setInterval(changeSize,1000);
+// setTimeout(clearInterval,10000,f2)
+
+// let diget = 0;
+// let flag = true
+// setInterval(()=>{
+//     if(flag){
+//         block.style.borderRadius = `${diget}%`
+//         diget += 10;
+//         if(diget >50) {
+//             flag=false}}
+//             else{
+//                 diget -= 10;
+//                 block.style.borderRadius = `${diget}%`
+//                 if(diget === 0) {
+//                     flag = true;
+//                 }
+//             }
+// },1000
+// )
+// const parent_node=document.querySelector('.parent_node')
+// let div= document.createElement(`div`);
+//  div.innerHTML="<h2> Hello </h2>"
+
+// let stars= new Array (5);
+// stars.map(item=>{
+//     let star=document.createElement(`span`);
+//     star.innerHTML=`i class="fa-solid fa-star"></i>`;
+//     parent__star.append(star)
+//     console.log()
+// })
+
+// const parent__star=document.querySelector('.parent__star');
+// const arrStar=[
+    
+// ]
+
+// let changeColor=document.querySelector('changeColor');
+// changeColor.addEventListener('click',()=>{
+//     document.body.style.background`rgb($(Math.random()*255),$(Math.random()*255),${Math.random()*255})`
+// })
+
+// let str=`I love Hayastan`;
+
+
+
+
+// const xhr=new XMLHttpRequest();
+
+// xhr.open("Get","https://retoolapi.dev/xHJzvU/persons");
+// xhr.onload=()=>{
+//     if(xhr.status===200 && xhr.readyState===4){
+//       JSON.parse(xhr.response).map(person=>{
+//           let div=document.createElement('div');
+//           div.innerHTML=`<h2>${person.name}</h2>
+//           <p>${person.email}</p><p>${person.surname}</p>`
+//       })
+//     }
+// }
+// xhr.send()
+
+
+
+// const person_parent=document.querySelector('.person_parent')
+// const xhr=new XMLHttpRequest();
+
+// xhr.open("Get","https://retoolapi.dev/xHJzvU/persons");
+// xhr.onload=()=>{
+//     if(xhr.status===200 && xhr.readyState===4){
+//       JSON.parse(xhr.response).map(person=>{
+//           let div=document.createElement('div');
+//           div.innerHTML=`<h2>${person.name}</h2>
+//           <p>${person.email}</p><p>${person.surname}</p>`;
+//           person_parent.append(div)
+//       })
+//     }
+// }
+// xhr.onerror=()=>{
+//    console.log(xhr);
+// }
+// xhr.send();
+
+// arajin argument fulfilled vijak
+// erkrord argument rejected vijak
+// let a=20;
+// let promise=new Promise((resolve,rejected)=>{
+//     if(a==20){
+//         resolve('[{id:1,name:"hakob"}]')
+//     }else{
+//         rejected('sxallll')
+//     }
+// });
+// promise
+// .then((item)=>console.log(item))
+// .catch(error=>console.error(error))
+
+// fetch("https://retoolapi.dev/xHJzvU/persons")
+// .then((res)=>res.json())
+// .then(data=>data.map(person=>{
+//           let div=document.createElement('div');
+//           div.innerHTML=`<h2>${person.name}</h2>
+//           <p>${person.email}</p><p>${person.surname}</p>`;
+//           person_parent.append(div)
+//  }))
+// .catch(err=>console.error(err))
+
+
+// const request = fetch(`https://reqres.in/api/unknown?page=2`)
+// .then(res=>res.json())
+// .then (result=>console.log(result.data))
+// catch (err=> console.error(error))
+
+
+class Kinder{
+    constructor (bueno, delice,conuntry ){
+        this.bueno=bueno;
+        this.delice=delice;
+        this.conuntry=conuntry;
+    }
+
+}
+
+const chocolate= new Kinder(bueno, delice,color )
+
+
+
+
+
+
+
